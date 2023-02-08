92a3ea8f13dcae49b6555e55eb4ebbae7e4a7edb
Charlie Roberts <charliewoo1412@gmail.com>
2023-02-08 16:24:21 +0000

    Upload missing inheritance poly task

---
+++ Lab1/src/inheritancepolymorphism/RacingCar.java
@@ -0,0 +1,33 @@
+package inheritancepolymorphism;
+
+public class RacingCar extends Car {
+    public String driver;
+    public int turboFactor;
+
+    public RacingCar(String model, String driver, int turboFactor) {
+        super(model);
+        this.setDriver(driver);
+        this.setTurboFactor(turboFactor);
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public int getTurboFactor() {
+        return turboFactor;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public void setTurboFactor(int turboFactor) {
+        this.turboFactor = turboFactor;
+    }
+
+    public void accelerate(int seconds) {
+        super.accelerate(seconds);
+        this.setSpeed(this.getSpeed() * this.getTurboFactor());
+    }
+}
